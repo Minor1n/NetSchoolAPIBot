588347f6bea040c15fbe34e87344ddc4fc95a445
RINE0NE <108397997+RINE0NE@users.noreply.github.com>
2022-10-09 18:43:02 +0900

    123

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const { Telegraf } = require('telegraf');
-
+const NS = require("netschoolapi").default;
 const bot = new Telegraf('5776158150:AAFqQaGbhLY1_0JZMg0nGny7NbTrHdl7EGk');
-
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-
-
-
-
 bot.launch().then(console.log('Ready!'))
+
+const user = new NS({
+    origin: "https://region.obramur.ru/",
+    login: "КоропА",
+    password: "2e2r6t6y7u7i",
+    school: "МАОУ \"Алексеевская гимназия г.Благовещенска\"",
+});
+
+(async function () {
+    const info = await user.info();
+    console.log(info);
+    bot.on('sticker', (ctx) => ctx.reply(`${info.lastName}`));
+})();
+
+
+
+
+
+
+
