0305c6bb7f9b4219ba431a43dc72aaf4915854e6
RINE0NE <108397997+RINE0NE@users.noreply.github.com>
2022-10-11 15:03:10 +0900

    ania

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const { Telegraf } = require('telegraf');
+const cron = require('cron');
 const fs = require("fs");
 const NS = require("netschoolapi").default;
+const Context = require("netschoolapi");
 const bot = new Telegraf('5776158150:AAFqQaGbhLY1_0JZMg0nGny7NbTrHdl7EGk');
-bot.launch().then(console.log('Ready!'))
+bot.launch().then(()=>{
+    console.log('Ready!')
+})
 const users_ns = require('./memory/users.json');
 setInterval(()=>{
     fs.writeFileSync('./memory/users.json',JSON.stringify(users_ns, null, "\t"));
 }, 1000*5);
 
+const user = new NS({
+    origin: "https://region.obramur.ru/",
+    login: users_ns.user.id1693247078.login,
+    password: users_ns.user.id1693247078.password,
+    school: "МАОУ \"Алексеевская гимназия г.Благовещенска\"",
+});
 
 /*bot.command('login', (ctx) => {
     try{
@@ -53,44 +63,68 @@ function addUserPassword(msg, id) {
         password:msg,
     }
 }*/
+let arr0= users_ns.user.id1693247078.arrMarks;
+let arr = [];
 
-
-const user = new NS({
-    origin: "https://region.obramur.ru/",
-    login: "КоропА",
-    password: "2e2r6t6y7u7i",
-    school: "МАОУ \"Алексеевская гимназия г.Благовещенска\"",
-});
-
-(async function () {
-    const info = await user.info();
-    console.log(info);
-    bot.on('sticker', (ctx) => ctx.reply(`${info.lastName}`));
-})();
-
-(async function () {
-    const dates = new Date();
-    const diary = await user.diary({
-        start: new Date(`2022-10-${String(dates.getDate()-1)}`),
-        end: dates,
-    });
-    console.log(dates)
-    //console.log(diary.days[0].lessons);
-
-
-    for (let key in diary.days[0].lessons) {
-        if (diary.days[0].lessons[key].assignments.mark !== null){
-            console.log(diary.days[0].lessons[key].assignments.mark)
+setInterval(async function(){
+        const diary = await user.diary({
+            start: new Date(`2022-09-1`),
+            end: new Date(),
+        });
+        markPackage(diary)
+        let intersect = intersection();
+        if(intersect[0]){
+            sendMark(intersect)
         }
-        /*
-        for(let les in diary.days[0].lessons[key].assignments) {
-            console.log(les)
-        }*/
+    },1000*10)
+
+function sendMark(msg){
+    let marks = []
+    for (let i in msg) {
+        marks.push(`\n${msg[i].date} ${msg[i].lesson} ${msg[i].mark}`)
     }
+    bot.telegram.sendMessage(1693247078, `Вам выставили оценку: ${marks}`)
+}
 
-})();
+function intersection(){
+    const set = new Set();
+    for (const { id } of arr0) set.add(id);
+    const intersection = arr.filter(({ id }) => !set.has(id));
+    for(let i in intersection){
+        arr0.push(intersection[i]);
+    }
+    arr.length = 0;
+    return intersection;
+}
 
-
+function markPackage(diary) {
+    for(let key in diary.days) {
+        for (let key2 in diary.days[key].lessons) {
+            for (let key3 in diary.days[key].lessons[key2].assignments){
+                if (diary.days[key].lessons[key2].assignments[key3]){
+                    let d = diary.days[key].lessons[key2].assignments[key3];
+                    if (d.mark !== null){
+                        if(diary.days[key].lessons[key2].subject ==='Основы безопасности жизнедеятельности'){
+                            arr.push({
+                                id: d.id,
+                                date: diary.days[key]._date.slice(0,10),
+                                lesson: 'ОБЖ',
+                                mark: d.mark,
+                            });
+                        }else{
+                            arr.push({
+                                id: d.id,
+                                date: diary.days[key]._date.slice(0,10),
+                                lesson: diary.days[key].lessons[key2].subject,
+                                mark: d.mark,
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 
