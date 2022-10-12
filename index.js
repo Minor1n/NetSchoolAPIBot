7b1ae43f4267ccf5111273a1ac54de52a560a9e4
RINE0NE <108397997+RINE0NE@users.noreply.github.com>
2022-10-12 14:24:46 +0900

    123

--- index.js
+++ index.js
@@ -1,107 +1,106 @@
 const { Telegraf } = require('telegraf');
 const cron = require('node-cron');
 const fs = require("fs");
-const NS = require("netschoolapi").default;
+const NS = require("netschoolapi").Safe;
 const bot = new Telegraf('5776158150:AAFqQaGbhLY1_0JZMg0nGny7NbTrHdl7EGk');
-bot.launch().then(()=>{
-    console.log('Ready!')
-})
+bot.launch().then(()=>{console.log('Ready!')})
 const users_ns = require('./memory/users.json');
-setInterval(()=>{
-    fs.writeFileSync('./memory/users.json',JSON.stringify(users_ns, null, "\t"));
-}, 1000*5);
+setInterval(()=>{fs.writeFileSync('./memory/users.json',JSON.stringify(users_ns, null, "\t"));}, 1000*5);
 
-const user = new NS({
-    origin: "https://region.obramur.ru/",
-    login: users_ns.user.id1693247078.login,
-    password: users_ns.user.id1693247078.password,
-    school: "МАОУ \"Алексеевская гимназия г.Благовещенска\"",
-});
+bot.command('start', (ctx) => {
+    if(!users_ns.user[ctx.from.id]){
+        users_ns.user[ctx.from.id] = setUser(ctx.from.id)
+    }
+})
 
-/*bot.command('login', (ctx) => {
-    try{
-        if(!users_ns[ctx.message.from.id]){users_ns[ctx.message.from.id] = createUser()}
-        if(users_ns[ctx.message.from.id].login === null) {log1(ctx);}else
-        if(users_ns[ctx.message.from.id].password === null) {log2(ctx);}else
-        {ctx.reply('Вы уже в системе!')}
-    }catch(e){console.log(e.message);}
-});
-
-function createUser() {
-    return {
+function setUser(user) {
+    return{
+        id: user,
         login: null,
         password: null,
+        school: null,
+        arrMarks: [],
     }
 }
 
-function log1(ctx){
-    ctx.reply('Напишите свой логин от NetSchool...')
-    bot.on('text', msg=>{
-        users_ns[msg.message.from.id].login = addUserLogin(msg.message.text, msg.message.from.id);
-    })
-}
-
-function log2(ctx){
-    ctx.reply('Напишите свой пароль от NetSchool...')
-    bot.on('text', msg=>{
-        users_ns[msg.message.from.id].password = addUserPassword(msg.message.text, msg.message.from.id);
-    })
-}
-
-
-function addUserLogin(msg, id) {
-    return{
-        login:msg,
-        //password: users_ns[id].password,
-    }
-}
-
-function addUserPassword(msg, id) {
-    return{
-        //login: users_ns[id].login,
-        password:msg,
-    }
-}*/
-let arr0 = users_ns.user.id1693247078.arrMarks;
-let arr = [];
-
-cron.schedule('0 0-23 * * *',
-    async function(){
+/*setInterval(
+async function(){
+    for (let i in users_ns.user){
+        const user = new NS({
+            origin: "https://region.obramur.ru/",
+            login: users_ns.user[i].login,
+            password: users_ns.user[i].password,
+            school: users_ns.user[i].school,
+        });
+        let arr0 = users_ns.user[i].arrMarks;
+        let arr = [];
         let date = new Date()
         date.setMonth(date.getMonth() - 1)
+        await user.logIn()
         const diary = await user.diary({
             start: date,
             end: new Date(),
         });
-        markPackage(diary)
-        let intersect = intersection();
+        markPackage(diary, arr0, arr)
+        let intersect = intersection(arr0, arr);
         if(intersect[0]){
-            sendMark(intersect)
+            sendMark(intersect, users_ns.user[i].id)
         }
+        await user.logOut()
     }
-);
+}, 1000*10
+);*/
 
-function sendMark(msg){
+cron.schedule('0 0-23 * * *', async function(){
+    try{
+        for (let i in users_ns.user){
+            if(users_ns.user[i].login === null || users_ns.user[i].password === null || users_ns.user[i].school === null)return;
+            const user = new NS({
+                origin: "https://region.obramur.ru/",
+                login: users_ns.user[i].login,
+                password: users_ns.user[i].password,
+                school: users_ns.user[i].school,
+            });
+            let arr0 = users_ns.user[i].arrMarks;
+            let arr = [];
+            let date = new Date()
+            date.setMonth(date.getMonth() - 1)
+            await user.logIn()
+            const diary = await user.diary({
+                start: date,
+                end: new Date(),
+            });
+            await markPackage(diary, arr0, arr)
+            let intersect = await intersection(arr0, arr);
+            if(intersect[0]){
+                sendMark(intersect, users_ns.user[i])
+            }
+            await user.logOut()
+        }
+    }catch(e){console.log(e);}
+});
+
+function sendMark(msg, id){
     let marks = []
     for (let i in msg) {
         marks.push(`\n${msg[i].date} ${msg[i].lesson} ${msg[i].mark}`)
     }
-    bot.telegram.sendMessage(1693247078, `Вам выставили оценку: ${marks}`)
+    bot.telegram.sendMessage(id, `Вам выставили оценку: ${marks}`)
 }
 
-function intersection(){
+function intersection(arr0, arr) {
     const set = new Set();
     for (const { id } of arr0) set.add(id);
     const intersection = arr.filter(({ id }) => !set.has(id));
     arr0.length = 0
-    for(let i in intersection){
-        arr0.push(arr[i])
+    for(let i in arr){
+        arr0.push({id: arr[i].id})
     }
     arr.length = 0;
     return intersection;
 }
 
-function markPackage(diary) {
+function markPackage(diary,  arr0, arr) {
     for(let key in diary.days) {
         for (let key2 in diary.days[key].lessons) {
             for (let key3 in diary.days[key].lessons[key2].assignments){
@@ -129,8 +128,3 @@ function markPackage(diary) {
         }
     }
 }
-
-
-
-
-
